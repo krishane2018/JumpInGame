0951ef1c42cd1e709e3739afa326e3071c94f8ed
Kush Gopeechund <kush.09@outlook.com>
2019-10-21 12:55:18 -0400

    Fixed some errors

--- src/JumpIn.java
+++ src/JumpIn.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 import java.util.ArrayList;
 
 /**
@@ -13,8 +14,9 @@ public class JumpIn {
 	private ArrayList<JumpInListener> listeners;
 	private Parser parser;
 	private int level;
-	private final int NUM_ROW=5;
-	private final int NUM_COL=5;
+	private final int NUM_ROWS=5;
+	private final int NUM_COLUMNS=5;
+	private final Point[] HOLES = {new Point(0,0),new Point(2,2), new Point(0,4),new Point(4,0), new Point(4,4)}; 
 	
 	/**
 	 * 
@@ -62,7 +64,9 @@ public class JumpIn {
 		
 		boolean finished = false;
         while (! finished) {
-            Move move = parser.getAnimal(listeners);
+        	GameObject chosenAnimal = parser.getAnimal(listeners);
+    		String options = displayOptions(chosenAnimal);
+    		Move move = parser.confirmOption(options, chosenAnimal);
             finished = processCommand(move);
             System.out.println(gameBoard.toString());
         }
@@ -74,14 +78,134 @@ public class JumpIn {
         }
 	}
 	
+	
+	private String displayOptions (GameObject chosenAnimal){
+		
+		String output = "";
+		
+		if (chosenAnimal.getClass().getName().equals("Rabbit")) {
+			ArrayList<Point> rabbitOptions = new ArrayList<Point>();
+			boolean isJump = false;
+			for (int x = (int) (chosenAnimal.getCoordinate().getX()-1); x>-1; x--) {
+				if (gameBoard[(int) chosenAnimal.getCoordinate().getY()][x]!=null && 
+						!(gameBoard[(int) chosenAnimal.getCoordinate().getY()][x].getClass().getName().equals("RabbitHole"))) {
+					isJump = true;
+				}
+				else if (isJump == true){
+					rabbitOptions.add(new Point(x,(int) chosenAnimal.getCoordinate().getY()));
+					break;
+				}
+			}
+			
+			isJump = false;
+			for (int x = (int) (chosenAnimal.getCoordinate().getX()+1); x<NUM_COLUMNS; x++) {
+				if (gameBoard[(int) chosenAnimal.getCoordinate().getY()][x]!=null && 
+						!(gameBoard[(int) chosenAnimal.getCoordinate().getY()][x].getClass().getName().equals("RabbitHole"))) {
+					isJump = true;
+				}
+				else if (isJump == true){
+					rabbitOptions.add(new Point(x,(int) chosenAnimal.getCoordinate().getY()));
+					break;
+				}
+			}
+			
+			isJump = false;
+			for (int y = (int) (chosenAnimal.getCoordinate().getY()-1); y>-1; y--) {
+				if (gameBoard[y][(int) chosenAnimal.getCoordinate().getX()]!=null && 
+						!(gameBoard[y][(int) chosenAnimal.getCoordinate().getX()].getClass().getName().equals("RabbitHole"))) {
+					isJump = true;
+				}
+				else if (isJump == true){
+					rabbitOptions.add(new Point((int) chosenAnimal.getCoordinate().getX(),y));
+					break;
+				}
+			}
+			
+			isJump = false;
+			for (int y = (int) (chosenAnimal.getCoordinate().getY()+1); y<NUM_ROWS; y++) {
+				if (gameBoard[y][(int) chosenAnimal.getCoordinate().getX()]!=null && 
+						!(gameBoard[y][(int) chosenAnimal.getCoordinate().getX()].getClass().getName().equals("RabbitHole"))) {
+					isJump = true;
+				}
+				else if (isJump == true){
+					rabbitOptions.add(new Point((int) chosenAnimal.getCoordinate().getX(),y));
+					break;
+				}
+			}
+			
+			
+			for (Point point : rabbitOptions) {
+				output+="("+point.getX()+","+point.getY()+")\n";
+			}
+			
+		}
+		else {
+				ArrayList<Point[]> foxOptions = new ArrayList<Point[]>();
+				Fox chosenFox = (Fox)(chosenAnimal);
+				if (chosenFox.getDirection().equals("Horizontal")) {
+					for (int x = (int) (chosenFox.getCoordinate().getX()-1); x>-1; x--) {
+						if (gameBoard[(int) chosenFox.getCoordinate().getY()][x]==null) {
+							Point[] tempArray = {new Point(x,(int) chosenFox.getCoordinate().getY()),new Point(x+1,(int) chosenFox.getCoordinate().getY())};
+							foxOptions.add(tempArray);
+						}
+						else {
+							break;
+						}
+					}
+					for (int x = (int) (chosenFox.getCoordinate2().getX()+1); x<NUM_COLUMNS; x++) {
+						if (gameBoard[(int) chosenFox.getCoordinate().getY()][x]==null) {
+							Point[] tempArray = {new Point(x,(int) chosenFox.getCoordinate().getY()),new Point(x+1,(int) chosenFox.getCoordinate().getY())};
+							foxOptions.add(tempArray);
+						}
+						else {
+							break;
+						}
+					}
+				}
+				else {
+					for (int y = (int) (chosenFox.getCoordinate().getY()-1); y>-1; y--) {
+						if (gameBoard[y][(int) chosenFox.getCoordinate().getX()]==null) {
+							Point[] tempArray = {new Point((int) chosenFox.getCoordinate().getX(),y),new Point((int) chosenFox.getCoordinate().getX(),y+1)};
+							foxOptions.add(tempArray);
+						}
+						else {
+							break;
+						}
+					}
+					for (int y = (int) (chosenFox.getCoordinate2().getY()+1); y<NUM_ROWS; y++) {
+						if (gameBoard[y][(int) chosenFox.getCoordinate().getX()]==null) {
+							Point[] tempArray = {new Point((int) chosenFox.getCoordinate().getX(),y),new Point((int) chosenFox.getCoordinate().getX(),y+1)};
+							foxOptions.add(tempArray);
+						}
+						else {
+							break;
+						}
+					}
+				}
+				
+				for (Point[] points : foxOptions) {
+					output+="{";
+					for (Point point: points) {
+						output+="("+point.getX()+","+point.getY()+") ";
+					}
+					output=output.trim();
+					output+="}\n";
+				}
+		}
+		
+		return output;
+		
+	}
+	
 	/**
 	 * 
 	 * @return
 	 */
+	//fix
 	private boolean checkWin() {
 		boolean win=false;
 		for(int i = 0;i<listeners.size();i++) {
-			if(listeners.get(i).getName()=="Rabbit") {
+			if(listeners.get(i).getClass().getName()=="Rabbit") {
 				win = listeners.get(i).getStatus;
 			}
 		}
@@ -94,11 +218,15 @@ public class JumpIn {
 	 * @return
 	 */
 	private boolean processCommand(Move move) {
-		JumpInEvent event = new JumpInEvent(this,move.getGameObject,move.end.x,move.end.y);
-		gameBoard[move.initial.y][move.initial.x]=null;
-		gameBoard[move.end.y][move.end.x]=move.getGameObject;
+		if(move.getChosenAnimal().getName()=="Rabbit") {
+			JumpInEvent event = new JumpInEvent(this,move.getChosenAnimal(),move.getFinalLocation(),HOLES);
+		}else{
+			JumpInEvent event = new JumpInEvent(this,move.getChosenAnimal(),move.getFinalLocation(),HOLES);		//fix for fox
+		}
+		gameBoard[move.getInitialLocation().y][move.getInitialLocation().x]=null;
+		gameBoard[move.getFinalLocation().y][move.getFinalLocation().x]=move.getChosenAnimal();
 		for(int i = 0;i<listeners.size();i++) {
-			if(listeners.get(i).equals(move.getGameObject)) {
+			if(listeners.get(i).equals(move.getChosenAnimal())) {
 				listeners.get(i).handleEvent(event);
 			}
 		}
@@ -111,9 +239,9 @@ public class JumpIn {
 	 */
 	public String toString(){
 		String board ="";
-		for(int i=0;i<NUM_ROW;i++) {
+		for(int i=0;i<NUM_ROWS;i++) {
 			board += "---------------------\n";
-			for(int j=0;j<NUM_COL;j++) {
+			for(int j=0;j<NUM_COLUMNS;j++) {
 				board += "| ";
 				objectToString(j,i);
 			}
