b9a2879207daf427d31a934acbb604854f3ce4a7
Kush Gopeechund <kush.09@outlook.com>
2019-11-18 12:28:26 -0500

    test umpinevent

---
+++ src/TestJumpInEvent.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestJumpInEvent {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testConsrtructorNoParam() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testConsrtructor5Param() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConsrtructor7Param() {
+		fail("Not yet implemented");
+	}
+	
+	@Test 
+	void testGetHole() {
+		
+	}
+}
