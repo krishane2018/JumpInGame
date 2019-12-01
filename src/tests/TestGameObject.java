847db5987dd8e031a0db91198dfb71c7ea11f8de
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2019-11-30 19:30:29 -0500

    Update TestGameObject.java
    Add tests for getCoordinate, setCoordinate, getX1, getY1, getName, and setName.

--- src/tests/TestGameObject.java
+++ src/tests/TestGameObject.java
@@ -72,6 +72,39 @@ class TestGameObject {
 			    new GameObject(new Point(1,5));
 			  });	
 	}
+	
+	@Test testGetName() {
+		GameObject Mush1 = new GameObject(new Point(0, 1), "M1");
+		assertEquals("M1", Mush1.getName());
+	}
+	
+	@Test testSetName() {
+		GameObject Mush1 = new GameObject(new Point(0, 1), "M1");
+		Mush1.setName("M2");
+		assertEquals("M2", Mush1.getName());
+	}
+	
+	@Test testGetCoordinate() {
+		GameObject Mush1 = new GameObject(new Point(0, 1), "M1");
+		assertEquals(Mush1.getCoordinate(), new Point(0, 1));
+	}
+	
+	@Test testSetCoordinate() {
+		GameObject Mush1 = new GameObject(new Point(0, 1), "M1");
+		Mush1.setCoordinate(new Point(0,2));
+		assertEquals(Mush1.getCoordinate(), new Point(0,2));
+	}
+	
+	@Test testGetX1() {
+		GameObject Mush1 = new GameObject(new Point(0, 1), "M1");
+		assertEquals(Mush1.getX1(), 0);
+	}
+	
+	@Test testGetY1() {
+		GameObject Mush1 = new GameObject(new Point(0, 1), "M1");
+		assertEquals(Mush1.getY1(),1);
+	}
+	
 	@Test testToXML(){
 		GameObject Mush1 = new GameObject(new Point(0, 1), "M1");
 		assertEquals(Mush1.toXML(),"<name>M1</name>\n<x1>0</x1>\n<y1>1</y1>\n");
