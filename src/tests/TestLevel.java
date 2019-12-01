2cb13ec4ae011e29120dc87c9dfa276a7cc084cb
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2019-12-01 16:08:17 -0500

    Create TestLevel.java

---
+++ src/tests/TestLevel.java
@@ -0,0 +1,21 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.junit.jupiter.api.*;
+
+import source.Fox;
+import source.GameObject;
+import source.Rabbit;
+
+class TestLevel {
+
+@BeforeEach
+	void setUp() throws Exception {
+	}
+
+}
