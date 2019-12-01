1600f06a1b72d5a03d0bcd26f2168f5a0c94436f
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2019-12-01 17:43:37 -0500

    Create TestXMLHandler.java

---
+++ src/tests/TestXMLHandler.java
@@ -0,0 +1,21 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.*;
+
+import org.junit.jupiter.api.*;
+
+import source.Fox;
+import source.GameObject;
+import source.Rabbit;
+import source.XMLHandler;
+
+class TestXMLHandler {
+}
