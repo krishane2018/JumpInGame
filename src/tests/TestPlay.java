baba38791a38e70aa4d372943edd01acba5ca58b
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2019-12-02 15:20:57 -0500

    Create TestPlay.java

---
+++ src/tests/TestPlay.java
@@ -0,0 +1,19 @@
+  
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Point;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import source.GameObject;
+import source.JumpIn;
+import source.GameObjectFactory;
+import source.Play;
+
+class TestPlay {
+
+
+
+}
