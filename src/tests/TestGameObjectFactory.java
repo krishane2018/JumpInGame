4b9b14d1f0d026ab1b361963c3d46762d6067560
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2019-12-01 19:04:09 -0500

    Create TestGameObjectFactory.java

---
+++ src/tests/TestGameObjectFactory.java
@@ -0,0 +1,19 @@
+package tests;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import java.util.Queue;
+import org.junit.jupiter.api.Test;
+
+import source.Fox;
+import source.GameObject;
+import source.JumpIn;
+import source.Rabbit;
+import source.Level;
+import source.GameObjectFactory;
+
+class TestGameObjectFactory {
+}
