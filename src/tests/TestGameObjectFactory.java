35709d635b6c663da9d1a0e2d0d414e3a53a2867
NRA3110 <55705935+NRA3110@users.noreply.github.com>
2019-12-01 19:35:17 -0500

    Update TestGameObjectFactory.java
    Tests for foxMakerHelper and foxMaker

--- src/tests/TestGameObjectFactory.java
+++ src/tests/TestGameObjectFactory.java
@@ -16,4 +16,26 @@ import source.Level;
 import source.GameObjectFactory;
 
 class TestGameObjectFactory {
+	@BeforeEach
+	void setUp() throws Exception {
+		GameObjectFactory factory = new GameObjectFactory();
+	}
+	@Test
+	void testFoxMakerHelper3() {
+		assertEquals(factory.foxMakerHelper(3), 4);
+	}
+	@Test
+	void testFoxMakerHelper5() {
+		assertEquals(factory.foxMakerHelper(5), 4);
+	}
+	@Test
+	void testFoxMakerVertical() {
+		Fox fox = new Fox (new Point(0, 1), new Point(0, 2), "F1", "Vertical");
+		assertEqual(factory.foxMaker(new Point(0, 1), "Vertical", 1), fox);
+	}
+	@Test
+	void testFoxMakerHorizontal() {
+		Fox fox = new Fox (new Point(0, 1), new Point(1, 1), "F1", "Horizontal");
+		assertEqual(factory.foxMaker(new Point(0, 1), "Horizontal", 1), fox);
+	}
 }
